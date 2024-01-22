5eafcfc70db075dce9c3102a6947ebe856e4e4a0
이흔오 <gms245@naver.com>
2024-01-22 17:02:43 +0900

    create : reactapp

---
+++ front-end/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Router, Route, Link } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      start
+    </div>
+  );
+}
+
+export default App;
