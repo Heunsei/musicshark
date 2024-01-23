cf2fbe6fad136203172ec3806237d5c6092c516c
김동우 <hide4321@naver.com>
2024-01-24 01:07:11 +0900

    BACK FIX

--- Back/src/main/java/service/implementation/AuthServiceImpl.java
+++ Back/src/main/java/service/implementation/AuthServiceImpl.java
@@ -2,5 +2,24 @@ package service.implementation;
 
 import service.AuthService;
 
+
+@Service
 public class AuthServiceImpl implements AuthService {
+
+    @Autowired private UserRepository userRepository;
+
+    private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
+    public void signUp(SingUpRequestDto dto){
+
+        String nickname = dto.getNickname();
+        String password = dto.getPassword();
+        String gender = dto.getGender();
+        String birth = dto.getBirth();
+        String user_email = dto.getUserEmail();
+        String profile_image = dto.profileImage();
+
+        UserEntity userEntity = new  userEntity(dto)
+
+    }
 }
