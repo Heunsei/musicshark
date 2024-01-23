81f4099dcad4bea9ac1b159fc54e6dc2ef7b5793
kdw <hide4321@naver.com>
2024-01-23 12:16:19 +0900

    DB, Back Folder Create

---
+++ Back/src/main/java/config/CorsConfig.java
@@ -0,0 +1,11 @@
+package config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    // 다른 출처의 자원을 공유하도록 설정
+
+}
--- Resources/ssatudio.sql
+++ Resources/ssatudio.sql
@@ -1,8 +1,11 @@
 create database if not exists ssatudio;
+
 use ssatudio;
 
 -- 해당 테이블에서만 쓰는 변수는 테이블명 안붙이기 ? --
 -- 다른 테이블에서도 쓰는 변수는 테이블명 붙이기 ? --
+
+
 
 -- 사용자
 create table if not exists `User` (
@@ -45,7 +48,7 @@ create table if not exists `notice` (
     notice_date timestamp default now() NOT NULL COMMENT '작성일자',
     isDelete tinyint NOT NULL DEFAULT 0 COMMENT '삭제여부',
     notice_title varchar(50) NOT NULL COMMENT '제목',
-    CONSTRAINT `fk_channel_idx` FOREIGN KEY (channel_idx) REFERENCES `channel` (channel_idx)
+    CONSTRAINT `fk_notice_channel_idx` FOREIGN KEY (channel_idx) REFERENCES `channel` (channel_idx)
 );
 
 -- 친구
@@ -54,8 +57,8 @@ create table if not exists `friend` (
     from_user_idx int NOT NULL COMMENT '유저 인덱스',
     to_user_idx int NOT NULL COMMENT '친구 인덱스',
     are_friend tinyint NOT NULL COMMENT '친구 여부',
-    CONSTRAINT `fk_from_user_idx` FOREIGN KEY (from_user_idx) REFERENCES `user` (user_idx),
-    CONSTRAINT `fk_to_user_idx` FOREIGN KEY (to_user_idx) REFERENCES `user` (user_idx)
+    CONSTRAINT `fk_friend_from_user_idx` FOREIGN KEY (from_user_idx) REFERENCES `user` (user_idx),
+    CONSTRAINT `fk_friend_to_user_idx` FOREIGN KEY (to_user_idx) REFERENCES `user` (user_idx)
  ); 
  
 -- 노래
@@ -72,15 +75,15 @@ create table if not exists `perfectplay`(
     song_idx int NOT NULL COMMENT '노래 인덱스',
     date timestamp NOT NULL COMMENT '실행 날짜',
     clear tinyint NOT NULL COMMENT '클리어 여부',
-    CONSTRAINT `fk_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx),
-    CONSTRAINT `fk_song_idx` FOREIGN KEY (song_idx) REFERENCES `song` (song_idx)
+    CONSTRAINT `fk_perfectplay_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx),
+    CONSTRAINT `fk_perfectplay_song_idx` FOREIGN KEY (song_idx) REFERENCES `song` (song_idx)
 );
 
 -- 관심악기
 create table if not exists `instrument` (
 	user_idx int primary key auto_increment COMMENT '유저 인덱스',
     instrument varchar(20) NOT NULL COMMENT '악기 이름',
-    CONSTRAINT `fk_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx)
+    CONSTRAINT `fk_instrument_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx)
 );
 
 -- 채팅방
@@ -95,16 +98,16 @@ create table if not exists `chat` (
     content text NOT NULL COMMENT '내용',
     chat_time timestamp NOT NULL DEFAULT now() COMMENT '작성시간',
     user_idx int NOT NULL COMMENT '유저 인덱스',
-    CONSTRAINT `fk_chatroom_idx` FOREIGN KEY (chatroom_idx) REFERENCES `chatroom` (chatroom_idx),
-    CONSTRAINT `fk_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx)
+    CONSTRAINT `fk_chat_chatroom_idx` FOREIGN KEY (chatroom_idx) REFERENCES `chatroom` (chatroom_idx),
+    CONSTRAINT `fk_chat_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx)
 );
 
 -- 채널 채팅방
 create table if not exists `channelChatRoom` (
     chatroom_idx int primary key auto_increment COMMENT '채팅방 인덱스',
     channel_idx int NOT NULL COMMENT '채널 인덱스',
-    CONSTRAINT `fk_chatroom_idx` FOREIGN KEY (chatroom_idx) REFERENCES `chatroom` (chatroom_idx),
-    CONSTRAINT `fk_channel_idx` FOREIGN KEY (channel_idx) REFERENCES `channel` (channel_idx)
+    CONSTRAINT `fk_channelChatRoom_chatroom_idx` FOREIGN KEY (chatroom_idx) REFERENCES `chatroom` (chatroom_idx),
+    CONSTRAINT `fk_channelChatRoom_channel_idx` FOREIGN KEY (channel_idx) REFERENCES `channel` (channel_idx)
 );
 
 -- 게시판
@@ -116,7 +119,7 @@ create table if not exists `board` (
     board_count int NOT NULL DEFAULT 0 COMMENT '조회수',
     board_isDelete tinyint NOT NULL DEFAULT 0 COMMENT '삭제여부',
     board_date timestamp NOT NULL DEFAULT now() COMMENT '작성일자',
-    CONSTRAINT `fk_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx)
+    CONSTRAINT `fk_board_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx)
 );
 
 -- 댓글
@@ -127,18 +130,18 @@ create table if not exists `comment` (
    comment_date timestamp NOT NULL DEFAULT now() COMMENT '작성일자',
    comment_isDelete tinyint NOT NULL DEFAULT 0 COMMENT '삭제여부',
    user_idx int NOT NULL COMMENT '유저 인덱스',
-   CONSTRAINT `fk_board_idx` FOREIGN KEY (board_idx) REFERENCES `board` (board_idx),
-   CONSTRAINT `fk_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx)
+   CONSTRAINT `fk_comment_board_idx` FOREIGN KEY (board_idx) REFERENCES `board` (board_idx),
+   CONSTRAINT `fk_comment_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx)
 );
 
 -- 영상
-create table if not exists `channel_video` (
+create table if not exists `video` (
    video_idx int primary key auto_increment COMMENT '영상 인덱스',
    video_date timestamp NOT NULL DEFAULT now() COMMENT '생성일자',
    video_title varchar(20) NOT NULL COMMENT '제목',
    video_picture blob NOT NULL COMMENT '영상사진',
    user_idx int NOT NULL COMMENT '유저 인덱스',
-   CONSTRAINT `fk_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx)
+   CONSTRAINT `fk_video_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx)
 );
 
 -- 채널 영상
@@ -146,9 +149,9 @@ create table if not exists `channel_video` (
    channel_idx int NOT NULL COMMENT '채널 인덱스',
    video_idx int NOT NULL COMMENT '영상 인덱스',
    user_idx int NOT NULL COMMENT '유저 인덱스',
-   CONSTRAINT `fk_channel_idx` FOREIGN KEY (channel_idx) REFERENCES `channel` (channel_idx),
-   CONSTRAINT `fk_video_idx` FOREIGN KEY (video_idx) REFERENCES `video` (video_idx),
-   CONSTRAINT `fk_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx) -- 유저 인덱스가 왜 필여하지?
+   CONSTRAINT `fk_channel_video_channel_idx` FOREIGN KEY (channel_idx) REFERENCES `channel` (channel_idx),
+   CONSTRAINT `fk_channel_video_video_idx` FOREIGN KEY (video_idx) REFERENCES `video` (video_idx),
+   CONSTRAINT `fk_channel_video_user_idx` FOREIGN KEY (user_idx) REFERENCES `user` (user_idx) -- 유저 인덱스가 왜 필여하지?
 );
 
 
