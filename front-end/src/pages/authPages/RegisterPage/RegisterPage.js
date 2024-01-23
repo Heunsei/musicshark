cb0dd81feb99f3a058dc95c62f8c6110752b4fce
heunoh <gms245@naver.com>
2024-01-23 17:27:17 +0900

    frontend : add register input
    Change-Id: I3afcbda2223765aa65aef1b68a02c580367101db

--- front-end/src/pages/authPages/RegisterPage/RegisterPage.js
+++ front-end/src/pages/authPages/RegisterPage/RegisterPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AuthBox from '../../../components/AuthBox';
-
+import RegisterPageHeader from './RegisterPageHeader';
+import RegisterInput from './RegisterInput';
 
 const RegisterPage = () => {
     return (
         <AuthBox>
-            
+            <RegisterPageHeader/>
+            <RegisterInput />
         </AuthBox>
     );
 };
