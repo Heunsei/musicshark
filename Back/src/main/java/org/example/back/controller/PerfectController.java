08e5c52d9445d0739d6f6084146e0afa2b5f34ed
kimmin <kimmin5714@gmail.com>
2024-01-25 15:36:45 +0900

    init

--- Back/src/main/java/org/example/back/controller/PerfectController.java
+++ Back/src/main/java/org/example/back/controller/PerfectController.java
@@ -4,14 +4,20 @@ import static org.springframework.http.HttpStatus.*;
 
 import java.util.List;
 
+import org.apache.coyote.Response;
 import org.example.back.common.ApiResponse;
 import org.example.back.dto.response.PerfectplayResponseDto;
 import org.example.back.dto.response.SongResponseDto;
+import org.example.back.entity.PerfectplayEntity;
 import org.example.back.service.implementation.PerfectplayServiceImpl;
 import org.example.back.service.implementation.SongServiceImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
@@ -39,10 +45,9 @@ public class PerfectController {
 	}
 
 	// 퍼펙트플레이 기록 조회
-	@GetMapping("/{user_id}")
-	public ResponseEntity<ApiResponse> perfectplayResultByUserId() {
-		List<PerfectplayResponseDto> perfectplayResultList = perfectplayServiceImpl.perfectplayResult();
-
+	@GetMapping("/{userIdx}")
+	public ResponseEntity<ApiResponse> perfectplayResultByUserIdx(@PathVariable int userIdx) {
+		List<PerfectplayResponseDto> perfectplayResultList = perfectplayServiceImpl.perfectplayResult(userIdx);
 		ApiResponse apiResponse = ApiResponse.builder()
 			.message("조회 결과")
 			.status(OK.value())
@@ -52,4 +57,5 @@ public class PerfectController {
 	}
 
 	// 퍼펙트플레이 기록 저장
+
 }
