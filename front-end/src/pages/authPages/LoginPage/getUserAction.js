b43278f633b8600109a55ea5c80e9c5e3c869c86
heunoh <gms245@naver.com>
2024-02-01 17:30:40 +0900

    Fix : login logic
    Change-Id: Ia8c2cdec92e5a399bff3a466ecfda2c5f49042f0

---
+++ front-end/src/pages/authPages/LoginPage/getUserAction.js
@@ -0,0 +1,34 @@
+import axios from 'axios'
+import * as setUser from './../../../redux/store/userSlice'
+import { setLogin } from '../../../redux/store/loginSlice';
+
+const getUserAction = async (ACCESS_TOKEN, dispatch, navigate) => {
+    const URL = process.env.REACT_APP_API_URL
+    try {
+        const isLogin = true
+        dispatch(setLogin(isLogin))
+        axios({
+            method: 'get',
+            url: `${URL}/user/`,
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${ACCESS_TOKEN}`,
+            }
+        })
+            .then((res) => {
+                console.log('setUser 상태')
+                dispatch(setUser.setEmail(res.data.userEmail))
+                dispatch(setUser.setNickname(res.data.nickname))
+                dispatch(setUser.setGender(res.data.gender))
+                dispatch(setUser.setBrith(res.data.birth))
+                navigate('/group')
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    } catch(err) {
+        console.log(err)
+    }
+}
+
+export default getUserAction
