62cc4ae101944f936f1459c3db02d6c72ebba232
heunoh <gms245@naver.com>
2024-02-06 12:23:17 +0900

    merge navbar
    Change-Id: If9eb3b5b91b9e2160b57e4d6d79fd3c74c217dd7

--- front-end/src/components/Navbar.js
+++ front-end/src/components/Navbar.js
@@ -1,45 +1,75 @@
-import React from 'react';
-import Button from '@mui/material/Button'
+import * as React from 'react';
+import {Box, AppBar, Toolbar, Typography} from '@mui/material';
 import { useNavigate } from 'react-router-dom';
+// import NavButton from './NavButton';
+// import LogoButton from './LogoButton';
+import styles from './Buttons.module.css';
 
+export default function Navbar(){
 
-
-const NavButton=(props)=>{
-    const { label, id } = props
-    const navigate = useNavigate()
+        const navigate = useNavigate();
     
-    const handleMove = () => {
-        if(id==="nav_about"){
+        const navigateToAbout = () => {
             navigate('/about')
         }
-        else if(id==="nav_group"){
+        const navigateToGroup = () => {
             navigate('/group')
         }
-        else if(id==="nav_single"){
+        const navigateToSingle = () => {
             navigate('/single')
         }
-        else if(id==="nav_community"){
+        const navigateToCommunity = () => {
             navigate('/community')
         }
-        else if(id==="nav_mypage"){
+        const navigateToMyPage = () => {
+            navigate('/mypage')
+        }
+        const navigateToRegister = () => {
             navigate('/register')
         }
-    }
 
+        const navigateToSignIn = () => {
+            navigate('/login');
+            
+        };
+    
+        const navigateToSignOut = () => {
+            navigate('/');
+        };
     return(
-        <Button
-            sx={{
-                bgcolor:'#997B66',
-                color:'#ffffff',
-                fontSize:'18px',
-                fontWeight:'bold',
-                mx:5
-            }}
-            onClick={handleMove}
-            >
-                {label} 
-            </Button>
-    );
-};
+        <Box border>
+            <AppBar position="static" sx={{bgcolor: "#997B66", position:"fixed", boxShadow:'none'}} >
+                <Toolbar position="static" sx={{bgcolor: "#997B66", height:'120px', width:'100%', position:"fixed", boxShadow:'none'}}>
+                    
+                    <Typography variant="h4" component="div" sx={{flexGrow:1}}>
+                        <p className={styles.logoBtn}>악기상어</p>
+                        
+                    </Typography>
+                    
+                        
+                    <p className={styles.loginBtn} onClick={navigateToSignIn}>
+                        Login
+                    </p>
 
-export default NavButton;
+                        <p className={styles.navBtn} onClick={navigateToAbout}>
+                            악기상어 소개
+                        </p>
+                        <p className={styles.navBtn} onClick={navigateToGroup}>
+                            함께 연습하기
+                        </p>
+                        <p className={styles.navBtn} onClick={navigateToSingle}>
+                            혼자 연습하기
+                        </p>
+                        <p className={styles.navBtn} onClick={navigateToCommunity}>
+                            커뮤니티
+                        </p>
+                        <p className={styles.navBtn} onClick={navigateToMyPage}>
+                            마이페이지
+                        </p>
+
+                    {/* 로그인/로그아웃 배치 */}
+                </Toolbar>
+            </AppBar>
+        </Box>
+    );
+}
