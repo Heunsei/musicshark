b974bc5a574da72d3001aa91c7da50be5db76af9
kimmin <kimmin5714@gmail.com>
2024-02-11 00:35:18 +0900

    Fix: time 에러 수정
    Change-Id: Iaec488c8ba43e30172be596976722febb5f8a1d9

--- front-end/src/pages/singlePages/PerfectPlayPlayPage.js
+++ front-end/src/pages/singlePages/PerfectPlayPlayPage.js
@@ -1,28 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
-import PlayCircleFilledWhiteIcon from '@mui/icons-material/PlayCircleFilledWhite';
-import StopCircleIcon from '@mui/icons-material/StopCircle';
-import LogoutIcon from '@mui/icons-material/Logout';
-
 import { getSongDetailAction } from './actions/getSongDetailAction';
 import PlayScreen from './components/PerfectPlayPlay/PlayScreen';
-import styles from './PerfectPlayPlayPage.module.css'
 
 const PerfectPlayPlayPage = () => {
     const { song_idx } = useParams()
     const [songDetail, setSongDetail] = useState([])
-    const [isPlaying, setIsPlaying] = useState(false)
-
-    const startPlay = () => {
-        //setIsPlaying(true)
-        //console.log(isPlaying)
-        //startButtonClick();
-    }
-    const stopPlay = () => {
-        //setIsPlaying(false);
-        //stopButtonClick();
-    }
 
     useEffect(() => {
         const get = async () => {
@@ -34,7 +18,6 @@ const PerfectPlayPlayPage = () => {
 
     return (
         <PlayScreen />
-
     );
 };
 
