24dd25ce48ee05b1b2e0947184e4ec73d2c625c5
Ferrorist <34542316+Ferrorist@users.noreply.github.com>
2024-02-13 07:31:24 +0900

    저장
    Change-Id: I426cfae052a24ea27c5202926eadc0a84c253642

--- Back/src/main/java/org/example/back/KaKao/service/KakaoServiceImpl.java
+++ Back/src/main/java/org/example/back/KaKao/service/KakaoServiceImpl.java
@@ -15,6 +15,9 @@ import java.net.URL;
  * 참고)
  * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api
  *
+ *
+https://velog.io/@akskflwn/Rest-API-%ED%99%9C%EC%9A%A9%ED%95%9C-%EC%B9%B4%EC%B9%B4%EC%98%A4-%EC%86%8C%EC%85%9C-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84ReactSpring-boot
+ *
  */
 
 @Service
