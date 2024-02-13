afd677beb1f3c32ee1e4bcc8f9703fa836c4742b
ferrorist <ferrorist@naver.com>
2024-02-13 23:20:32 +0900

    오타 수정
    Change-Id: I2d8cec462a6eedaca370c84805989e35ad328d57

--- front-end/src/pages/authPages/LoginPage/KakaoLoginButton.js
+++ front-end/src/pages/authPages/LoginPage/KakaoLoginButton.js
@@ -1,7 +1,6 @@
 const kakaoLoginAPI = `https://kauth.kakao.com/oauth/authorize
 ?client_id=${process.env.REACT_APP_KAKAO_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}
 &response_type=code`;
-// 결과 예시: http://localhost:3000/auth/kakao?code=kyMxePa3bkiEoZB3FYypJAdWRYnPAE5-3pms9nlpop_GbiPHHZ2Boi1TmUoKPXTaAAABjZtKG5hyxKx5jTsi9A
 
 export const KakaoLoginButton = () => {
     return (
