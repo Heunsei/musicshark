98c895e341d29752c5de7067d131952acd7f6315
ferrorist <ferrorist@naver.com>
2024-02-13 22:47:41 +0900

    change: frontend .env 변수 수정
    Change-Id: Ic15109abbb9546d2fde76d8e036c9183da2dda41

--- Back/src/main/java/org/example/back/KaKao/service/KakaoServiceImpl.java
+++ Back/src/main/java/org/example/back/KaKao/service/KakaoServiceImpl.java
@@ -31,14 +31,11 @@ import com.google.gson.JsonParser;
 
 import lombok.RequiredArgsConstructor;
 
-
-
 /*
  * 참고)
  * https://developers.kakao.com/docs/latest/ko/kakaologin/rest-api
  *
- *
-https://velog.io/@akskflwn/Rest-API-%ED%99%9C%EC%9A%A9%ED%95%9C-%EC%B9%B4%EC%B9%B4%EC%98%A4-%EC%86%8C%EC%85%9C-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84ReactSpring-boot
+ * https://velog.io/@akskflwn/Rest-API-%ED%99%9C%EC%9A%A9%ED%95%9C-%EC%B9%B4%EC%B9%B4%EC%98%A4-%EC%86%8C%EC%85%9C-%EB%A1%9C%EA%B7%B8%EC%9D%B8-%EA%B5%AC%ED%98%84ReactSpring-boot
  *
  */
 
