7cf4613be372a434f4d5f31046b86942593f71c0
heunoh <gms245@naver.com>
2024-02-14 15:30:23 +0900

    Add : kakao login btn

--- front-end/src/pages/authPages/LoginPage/KakaoLoginButton.js
+++ front-end/src/pages/authPages/LoginPage/KakaoLoginButton.js
@@ -1,13 +1,24 @@
+import { useNavigate, Link } from "react-router-dom";
+import styles from './KakaoLoginButton.module.css'
+import ChatBubbleIcon from '@mui/icons-material/ChatBubble';
+
 const kakaoLoginAPI = `https://kauth.kakao.com/oauth/authorize
 ?client_id=${process.env.REACT_APP_KAKAO_API_KEY}&redirect_uri=${process.env.REACT_APP_KAKAO_REDIRECT_URI}
 &response_type=code`;
 
 export const KakaoLoginButton = () => {
+    const handleButtonClick = () => {
+        window.location.href = kakaoLoginAPI
+    }
+
     return (
         <>
-            <a href={kakaoLoginAPI}>
-                <img src={process.env.PUBLIC_URL + '/button/kakao_login_medium_wide.png'} />
-            </a>
+            <button onClick={handleButtonClick} className={styles.kakaoLoginBtn}>
+                <div style={{ display: 'flex', alignContent: 'center', justifyContent: 'center' }}>
+                    <ChatBubbleIcon />
+                </div>
+                <p style={{ margin: '0 0 0 5px', padding: '0px' }}>카카오 로그인</p>
+            </button>
         </>
     );
 };
